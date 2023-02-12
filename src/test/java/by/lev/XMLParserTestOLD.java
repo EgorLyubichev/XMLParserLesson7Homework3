7e4dd71cea53a812eeb0b60f4abd6f13afcfabbc
Egor Lyubichev <EgorLyubichev@mail.ru>
2023-02-12 10:21:18 +0300

    Returned to orifinal form

--- src/test/java/by/lev/XMLParserTestOLD.java
+++ src/test/java/by/lev/XMLParserTestOLD.java
@@ -14,7 +14,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-public class XMLParserTest {
+public class XMLParserTestOLD {
 
     @Test
     public void showMeYourLines() {
